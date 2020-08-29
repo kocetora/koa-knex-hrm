8e91d296dbd5cef04466657e7d70b818a4fba524
kocetora <kocetora@gmail.com>
2020-08-29 13:46:39 +0300

    ready

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 'use strict';
 const Koa = require('koa');
+const passport = require('koa-passport');
+const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
 const router = require('./router/router');
 const PORT = process.env.PORT || 3000;
 
 const app = new Koa();
+app.use(cors());
 app.use(bodyParser());
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/jwtStrategy')(passport);
 app.use(router.routes());
 app.use(router.allowedMethods());
 
