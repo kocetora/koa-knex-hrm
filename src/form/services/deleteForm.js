9262f85b632a0265dcbda81fc5a3fbae064fb422
kocetora <kocetora@gmail.com>
2020-09-02 14:08:58 +0300

    Ready

--- src/form/services/deleteForm.js
+++ src/form/services/deleteForm.js
@@ -1,10 +1,8 @@
 'use strict';
 const queries = require('../queries/index');
 
-const deleteForm = async(
-  formid
-) => {
-  console.log(formid)
+const deleteForm = async formid => {
+  console.log(formid);
   const [form] = await queries.getForm(formid);
   if (!form) {
     throw new Error('Form with this id doesn\'t exists');
