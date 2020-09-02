65a800724488bc690b728f448c5eb66fb83a6bb0
kocetora <kocetora@gmail.com>
2020-09-02 14:06:54 +0300

    wip

--- src/form/queries/index.js
+++ src/form/queries/index.js
@@ -1,16 +1,22 @@
 'use strict';
-const { createComment } = require('./createComment');
-const { createForm } = require('./createForm');
+const { addComment } = require('./addComment');
+const { addForm } = require('./addForm');
+const { addProfessions } = require('./addForm');
+const { addMessengers } = require('./addForm');
+const { addLanguageSkills } = require('./addForm');
 const { deleteForm } = require('./deleteForm');
 const { getComments } = require('./getComments');
 const { updateForm } = require('./updateForm');
 const { getForm } = require('./getForm');
 
 module.exports = {
-  createComment,
-  createForm,
+  addComment,
+  addForm,
   updateForm,
   getComments,
   deleteForm,
   getForm,
+  addProfessions,
+  addLanguageSkills,
+  addMessengers,
 };
