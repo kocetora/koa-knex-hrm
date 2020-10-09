34cedb79c3ca1c1aa831cd2b1d96c3c1f2ce8b27
kocetora <kocetora@gmail.com>
2020-10-09 10:35:04 +0300

    test: placeholder for tests

---
+++ src/form/tests/comment/get.test.js
@@ -0,0 +1,20 @@
+'use strict';
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const chaiMatchPattern = require('chai-match-pattern');
+chai.use(chaiHttp);
+chai.use(chaiMatchPattern);
+
+describe('LOGIN', () => {
+  it('LOGIN CORRECT DATA 200', (done) => {
+    chai
+      .request('http://localhost:3000')
+      .post('/login')
+      .type('form')
+      .set('content-type', 'application/json')
+      .send()
+      .end(() => {
+        done();
+      });
+  });
+});
