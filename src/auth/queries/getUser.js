c9663ada1b614ae73b08e5c8b23d9e12c8fc8112
Lint Action <lint-action@samuelmeuli.com>
2020-10-16 10:13:59 +0000

    fix: code style issues with Prettier

--- src/auth/queries/getUser.js
+++ src/auth/queries/getUser.js
@@ -1,7 +1,7 @@
 'use strict';
 const knex = require('../../db/knex');
 
-const getUser = username => knex('users').where({ username }).select();
+const getUser = (username) => knex('users').where({ username }).select();
 
 module.exports = {
   getUser,
