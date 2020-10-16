5ffe9b5427177919a93e8a495a35355c95bdf8e1
Lint Action <lint-action@samuelmeuli.com>
2020-10-16 11:02:29 +0000

    fix: code style issues with ESLint

--- src/form/services/addComment.js
+++ src/form/services/addComment.js
@@ -5,7 +5,7 @@ const addComment = async (formid, { userid, comment }) => {
   const [form] = await queries.getForm(formid);
   const [user] = await queries.getUser(userid);
   if (!form || !user) {
-    throw new Error("Form or user with this id doesn't exists");
+    throw new Error('Form or user with this id doesn\'t exists');
   }
   await queries.addComment({ userid, formid, comment });
 };
