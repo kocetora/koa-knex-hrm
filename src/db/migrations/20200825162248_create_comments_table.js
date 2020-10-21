47f6dbda9a7fe664f63b42711425d1753175779c
kocetora <kocetora@gmail.com>
2020-10-21 12:50:08 +0300

    chore: added timestamp to comment table

--- src/db/migrations/20200825162248_create_comments_table.js
+++ src/db/migrations/20200825162248_create_comments_table.js
@@ -1,11 +1,12 @@
 'use strict';
 
-exports.up = function (knex) {
+exports.up = function(knex) {
   return knex.schema.createTable('comments', (table) => {
     table.increments('id').unsigned().primary();
     table.string('comment').notNullable();
     table.integer('userid').unsigned().references('users.id');
     table.integer('formid').unsigned();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
     table
       .foreign('formid')
       .references('forms.id')
@@ -14,6 +15,6 @@ exports.up = function (knex) {
   });
 };
 
-exports.down = function (knex) {
+exports.down = function(knex) {
   return knex.schema.dropTable('comments');
 };
