1b5e2437ef4b33ebcb2a03f37675532ff89fc426
kocetora <kocetora@gmail.com>
2020-11-05 11:54:21 +0200

    test: added stryker, configured mutation testing

--- test/base.get.test.js
+++ test/base.get.test.js
@@ -1,44 +1,30 @@
 'use strict';
 // eslint-disable-next-line
-const app = require('../../../app');
+const app = require('../app');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const chaiMatchPattern = require('chai-match-pattern');
 chai.use(chaiHttp);
 chai.use(chaiMatchPattern);
-const knex = require('../../db/knex');
+const knex = require('../src/db/knex');
 const { expect } = require('chai');
 
-describe('DELETE FORM', () => {
+describe('GET FORMS', () => {
   before(done => {
     knex.migrate
       .latest()
       .then(() => knex.seed.run())
       .then(() => done());
   });
-
   it('200 SUCCESS', done => {
     chai
       .request('http://localhost:3000/v1')
-      .delete('/form/2147483646')
+      .get('/base/')
       .type('form')
       .set('content-type', 'application/json')
       .send()
       .end((error, res) => {
         expect(res).to.have.status(200);
-        done();
-      });
-  });
-
-  it('404 form not found', done => {
-    chai
-      .request('http://localhost:3000/v1')
-      .delete('/form/19490')
-      .type('form')
-      .set('content-type', 'application/json')
-      .send()
-      .end((error, res) => {
-        expect(res).to.have.status(404);
         done();
       });
   });
