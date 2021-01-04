36121ba55c534864c2b6238f300cfe5360bc5ac6
kocetora <kocetora@gmail.com>
2021-01-04 03:54:03 +0200

    feat: admins can create another admins

--- src/auth/controllers/index.js
+++ src/auth/controllers/index.js
@@ -1,10 +1,12 @@
 'use strict';
 const { register } = require('./register');
+const { registerAdmin } = require('./registerAdmin');
 const { login } = require('./login');
 const { logout } = require('./logout');
 
 module.exports = {
   register,
+  registerAdmin,
   login,
   logout,
 };
--- src/auth/services/index.js
+++ src/auth/services/index.js
@@ -1,8 +1,10 @@
 'use strict';
 const { register } = require('./register');
+const { registerAdmin } = require('./registerAdmin');
 const { login } = require('./login');
 
 module.exports = {
+  registerAdmin,
   register,
   login,
 };
