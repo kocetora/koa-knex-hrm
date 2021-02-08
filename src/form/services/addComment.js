2b5f28c25158da435f1be258511490c576c7ca8a
kocetora <kocetora@gmail.com>
2021-02-08 02:53:34 +0200

    chore: ready

--- src/form/services/addComment.js
+++ src/form/services/addComment.js
@@ -1,9 +1,10 @@
 'use strict';
 const queries = require('../queries/index');
 
-const addComment = async (formid, { userid, username, comment }) => {
+const addComment = async (formid, { userid, comment }) => {
   const [form] = await queries.getForm(formid);
   const [user] = await queries.getUser(userid);
+  const username = user.username;
   if (!form || !user) {
     const record = form ? 'User' : 'Form';
     const error = new Error(record + ' with this id doesn\'t exist');
