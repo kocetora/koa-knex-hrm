e68605f6230e9309ab43e97685b8ec5fd9fe56b0
kocetora <kocetora@gmail.com>
2021-05-11 11:34:11 +0300

    ci: ci

--- knexfile.js
+++ knexfile.js
@@ -18,17 +18,12 @@ module.exports = {
   },
   ci: {
     client: 'pg',
-    connection: {
-      host: process.env.POSTGRES_HOST,
-      user: process.env.POSTGRES_PORT,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/src/db/migrations',
     },
     seeds: {
-      directory: __dirname + '/src/db/seeds/test',
+      directory: __dirname + '/src/db/seeds/prod',
     },
   },
   production: {
