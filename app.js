3f13403819c070e2dfef578b17900fd9d489786b
Victoria Rieznikova <43138920+kocetora@users.noreply.github.com>
2021-12-20 17:58:02 +0200

    cleared commented

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-// 'use strict';
-// const Koa = require('koa');
-// const passport = require('koa-passport');
-// const Router = require('koa-router');
-// const cors = require('@koa/cors');
-// const bodyParser = require('koa-bodyparser');
-// const PORT = process.env.PORT || 4000;
-
-// const app = new Koa();
-// app.use(cors());
-// app.use(bodyParser({ jsonLimit: '50mb' }));
-// const router = new Router();
-
-// const authRouter = require('./src/auth/router');
-
-// router.use(authRouter.routes());
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(router.routes());
-// app.use(router.allowedMethods());
-
-// const server = app.listen(PORT, () => console.log(`🌍 Server listening on port ${PORT}`));
-
-// const stop = () => {
-//   server.close();
-// };
-
-// module.exports = server;
-// module.exports.stop = stop;
-
 'use strict';
 const Koa = require('koa');
 const https = require('https');
