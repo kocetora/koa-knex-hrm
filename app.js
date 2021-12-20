250a3b9f80c083a4445d335af79d9469c38a7021
Victoria Rieznikova <43138920+kocetora@users.noreply.github.com>
2021-12-20 17:55:25 +0200

    setted certificates

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
+// 'use strict';
+// const Koa = require('koa');
+// const passport = require('koa-passport');
+// const Router = require('koa-router');
+// const cors = require('@koa/cors');
+// const bodyParser = require('koa-bodyparser');
+// const PORT = process.env.PORT || 4000;
+
+// const app = new Koa();
+// app.use(cors());
+// app.use(bodyParser({ jsonLimit: '50mb' }));
+// const router = new Router();
+
+// const authRouter = require('./src/auth/router');
+
+// router.use(authRouter.routes());
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(router.routes());
+// app.use(router.allowedMethods());
+
+// const server = app.listen(PORT, () => console.log(`🌍 Server listening on port ${PORT}`));
+
+// const stop = () => {
+//   server.close();
+// };
+
+// module.exports = server;
+// module.exports.stop = stop;
+
 'use strict';
 const Koa = require('koa');
+const https = require('https');
+const fs = require('fs');
 const passport = require('koa-passport');
 const Router = require('koa-router');
 const cors = require('@koa/cors');
@@ -7,6 +39,12 @@ const bodyParser = require('koa-bodyparser');
 const PORT = process.env.PORT || 4000;
 
 const app = new Koa();
+
+const options = {
+  key: fs.readFileSync('localhost-key.pem'),
+  cert: fs.readFileSync('localhost.pem'),
+};
+
 app.use(cors());
 app.use(bodyParser({ jsonLimit: '50mb' }));
 const router = new Router();
@@ -19,7 +57,7 @@ app.use(passport.session());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-const server = app.listen(PORT, () => console.log(`🌍 Server listening on port ${PORT}`));
+const server = https.createServer(options, app.callback()).listen(PORT, () => console.log(`🌍 Server listening on port ${PORT}`));
 
 const stop = () => {
   server.close();
